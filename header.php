57e3c66cf9f8ad9197ad3db3703ad1e0694a2f4c
vicky sharma <vickysharma567890@gmail.com>
2026-02-02 13:19:15 +0530

    header updated

--- header.php
+++ header.php
@@ -1,69 +1,64 @@
-
-
-<header class="main-header clearfix">
-            <div class="main-header__top">
-                <div class="container">
-                    <div class="main-header__top-inner clearfix">
-                        <div class="main-header__top-left">
-                            <div class="main-header__top-logo">
-                               <a href="index.php">
-  <img src="assets/images/resources/logo-1ac.pn" alt="" style="width: 150px;">
-</a>
-
+        <header class="main-header-two clearfix">
+            <div class="main-header-two__top clearfix">
+                <div class="main-header-two__top-inner clearfix">
+                    <div class="main-header-two__top-left">
+                        <div class="main-header-two__top-request">
+                            <div class="main-header-two__social">
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
                             </div>
                         </div>
-                        <div class="main-header__top-right">
-                            <ul class="list-unstyled main-header__top-contact">
-                                <li>
-                                    <div class="icon">
-                                        <span class="icon-location"></span>
-                                    </div>
-                                    <div class="content">
-                                        <p>Quick Location</p>
-                                        <h5>3/15, Fatehpuri Chowk</h5>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="icon">
-                                        <span class="icon-email-1"></span>
-                                    </div>
-                                    <div class="content">
-                                        <p>Quick Email Now!</p>
-                                        <h5><a href="">sanjay1966rd@gmail.com</span></a></h5>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="icon">
-                                        <span class="icon-phone"></span>
-                                    </div>
-                                    <div class="content">
-                                        <p>Phone Number</p>
-                                        <h5><a href="tel:9811217586">+919811217586</a></h5>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
+                    </div>
+                    <div class="main-header-two__top-right">
+                        <ul class="list-unstyled main-header-two__top-contact">
+                            <li>
+                                <div class="icon">
+                                    <span class="icon-email-1"></span>
+                                </div>
+                                <div class="content">
+                                    <p><a href="">sanjay1966rd@gmail.com</span></a></p>
+                                </div>
+                            </li>
+                            <li>       
+                            <li>
+                                <div class="icon">
+                                    <span class="icon-phone"></span>
+                                </div>
+                                <div class="content">
+                                    <p><a href="tel:9811217586">+919811217586</a></p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <span class="icon-location"></span>
+                                </div>
+                                <div class="content">
+                                    <p>3/15, Fatehpuri Chowk</p>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
-            <nav class="main-menu clearfix">
-                <div class="main-menu__wrapper clearfix">
-                    <div class="container">
-                        <div class="main-menu__wrapper-inner clearfix">
-                            <div class="main-menu__left">
-                                <div class="main-menu__main-menu-box">
-                                    <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
-                                    <ul class="main-menu__list">
-                                        <li class="current">
-                                            <a href="index.php">Home </a>
-                                        </li>
-                                         <li>
-                                            <a href="about.php">About</a>
-
-                                        </li>
-                                        <li class="dropdown">
-                                            <a href="#">Services</a>
-                                            <ul>
+            <nav class="main-menu main-menu-two clearfix">
+                <div class="main-menu-two__wrapper clearfix">
+                    <div class="main-menu-two__left">
+                        <div class="main-menu-two__logo">
+                            <a href="index.html"><img src="assets/images/resources/logo-1.png" alt=""></a>
+                        </div>
+                        <div class="main-menu-two__main-menu-box">
+                            <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
+                            <ul class="main-menu__list">
+                                <li class="current">
+                                    <a href="index.php">Home </a>
+                                </li>
+                                <li>
+                                    <a href="about.php">About</a>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#">Services</a>
+                                    <ul>
   <li>
     <a href="wooden-mouldings.php">Wooden Mouldings</a>
   </li>
@@ -88,9 +83,9 @@
     <a href="crystal-frames.php">Crystal Frames</a>
   </li>
 </ul>
-
-                                        </li>
-                                        <li>
+                                </li>
+                                
+                               <li>
                                             <a href="gallery.php">Gallery</a>
                                         </li>
                                         <!-- <li>
@@ -99,18 +94,24 @@
                                         <li>
                                             <a href="contact.php">Contact</a>
                                         </li>
-                                    </ul>
-                                </div>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="main-menu-two__right">
+                        <div class="main-menu-two__call">
+                            <div class="main-menu-two__call-icon">
+                                <span class="icon-phone"></span>
                             </div>
-                            <div class="main-menu__right">
-                                <div class="main-menu__search-btn">
-                                    <div class="main-menu__search-box">
-                                        <a href="#" class="main-menu__search search-toggler icon-search-1"></a>
-                                    </div>
-                                    <div class="main-menu__btn-box">
-                                        <a href="contact.php">Request Book</a>
-                                    </div>
-                                </div>
+                            <div class="main-menu-two__call-content">
+                                <p class="main-menu-two__call-sub-title">Phone Number</p>
+                                <h4 class="main-menu-two__call-number"><a href="tel:88001152">(88) 00-11-52 Call</a>
+                                </h4>
+                            </div>
+                        </div>
+                        <div class="main-menu-two__social-btn">
+                            
+                            <div class="main-menu-two__btn">
+                                <a href="contact.php">Appointment</a>
                             </div>
                         </div>
                     </div>
@@ -118,6 +119,62 @@
             </nav>
         </header>
 
-        <div class="stricky-header stricked-menu main-menu">
+        <div class="stricky-header stricked-menu main-menu main-menu-two">
             <div class="sticky-header__content"></div><!-- /.sticky-header__content -->
         </div><!-- /.stricky-header -->
+
+        <div class="mobile-nav__wrapper">
+        <div class="mobile-nav__overlay mobile-nav__toggler"></div>
+        <!-- /.mobile-nav__overlay -->
+        <div class="mobile-nav__content">
+            <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
+
+            <div class="logo-box">
+                <a href="index.html" aria-label="logo image"><img src="assets/images/resources/footer-logo.png"
+                        width="143" alt="" /></a>
+            </div>
+            <!-- /.logo-box -->
+            <div class="mobile-nav__container"></div>
+            <!-- /.mobile-nav__container -->
+
+            <ul class="mobile-nav__contact list-unstyled">
+                <li>
+                    <i class="fa fa-envelope"></i>
+                    <a href="https://themazine.com/cdn-cgi/l/email-protection#b8d6dddddcd0ddd4c8f8c8d9dbd3d9dfddf6d9d5dde7e796dbd7d5"><span class="__cf_email__" data-cfemail="98f6fdfdfcf0fdf4e8d8eff7f7fce2f9b6fbf7f5">[email&#160;protected]</span></a>
+                </li>
+                <li>
+                    <i class="fa fa-phone-alt"></i>
+                    <a href="tel:666-888-0000">666 888 0000</a>
+                </li>
+            </ul><!-- /.mobile-nav__contact -->
+            <div class="mobile-nav__top">
+                <div class="mobile-nav__social">
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-facebook-square"></a>
+                    <a href="#" class="fab fa-pinterest-p"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                </div><!-- /.mobile-nav__social -->
+            </div><!-- /.mobile-nav__top -->
+
+
+
+        </div>
+        <!-- /.mobile-nav__content -->
+    </div>
+    <!-- /.mobile-nav__wrapper -->
+
+    <div class="search-popup">
+        <div class="search-popup__overlay search-toggler"></div>
+        <!-- /.search-popup__overlay -->
+        <div class="search-popup__content">
+            <form action="#">
+                <label for="search" class="sr-only">search here</label><!-- /.sr-only -->
+                <input type="text" id="search" placeholder="Search Here..." />
+                <button type="submit" aria-label="search submit" class="thm-btn">
+                    <i class="icon-search-1"></i>
+                </button>
+            </form>
+        </div>
+        <!-- /.search-popup__content -->
+    </div>
+    <!-- /.search-popup -->
--- index.php
+++ index.php
@@ -1363,70 +1363,7 @@
     </div>
     <!-- /.page-wrapper -->
 
-    <div class="mobile-nav__wrapper">
-      <div class="mobile-nav__overlay mobile-nav__toggler"></div>
-      <!-- /.mobile-nav__overlay -->
-      <div class="mobile-nav__content">
-        <span class="mobile-nav__close mobile-nav__toggler"
-          ><i class="fa fa-times"></i
-        ></span>
-
-        <div class="logo-box">
-          <a href="index.php" aria-label="logo image"
-            ><img
-              src="assets/images/resources/footer-logo1.png"
-              width="143"
-              alt=""
-          /></a>
-        </div>
-        <!-- /.logo-box -->
-        <div class="mobile-nav__container"></div>
-        <!-- /.mobile-nav__container -->
-
-        <ul class="mobile-nav__contact list-unstyled">
-          <li>
-            <i class="fa fa-envelope"></i>
-            <a href="mailto:sanjay1966rd@gmail.com">
-  sanjay1966rd@gmail.com
-</a>
-          </li>
-          <li>
-            <i class="fa fa-phone-alt"></i>
-            <a href="tel:+91 98112 17586">+91 98112 17586</a>
-          </li>
-        </ul>
-        <!-- /.mobile-nav__contact -->
-        <div class="mobile-nav__top">
-          <div class="mobile-nav__social">
-            <a href="#" class="fab fa-twitter"></a>
-            <a href="#" class="fab fa-facebook-square"></a>
-            <a href="#" class="fab fa-pinterest-p"></a>
-            <a href="#" class="fab fa-instagram"></a>
-          </div>
-          <!-- /.mobile-nav__social -->
-        </div>
-        <!-- /.mobile-nav__top -->
-      </div>
-      <!-- /.mobile-nav__content -->
-    </div>
-    <!-- /.mobile-nav__wrapper -->
-
-    <div class="search-popup">
-      <div class="search-popup__overlay search-toggler"></div>
-      <!-- /.search-popup__overlay -->
-      <div class="search-popup__content">
-        <form action="#">
-          <label for="search" class="sr-only">search here</label
-          ><!-- /.sr-only -->
-          <input type="text" id="search" placeholder="Search Here..." />
-          <button type="submit" aria-label="search submit" class="thm-btn">
-            <i class="icon-search-1"></i>
-          </button>
-        </form>
-      </div>
-      <!-- /.search-popup__content -->
-    </div>
-    <!-- /.search-popup -->
+    
 
     <a href="#" data-target="html" class="scroll-to-target scroll-to-top"
       ><i class="fa fa-angle-up"></i
